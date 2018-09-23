8929248fae3cfe4dddde3ced1bb6ff29debbd7eb
Shweta Kumari <201552042@iiitvadodara.ac.in>
2018-09-23 10:09:04 +0530

    Revert "'password'"

--- fest/kreiva/views.py
+++ fest/kreiva/views.py
@@ -1,41 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from . import forms
 #
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
+
 # Create your views here.
 def index(request):
     return render(request, 'kreiva/index.html')
 
 def register(request):
     registered = False
-
     if request.method == 'POST':
         user_form = forms.UserForm(data=request.POST)
-
-        if user_form.is_valid()  and user_form.cleaned_data['password'] == user_form.cleaned_data['confirm_password']:
+        profile_form = forms.UserProfileInfoForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-
-
-
-
-            return redirect('/kreiva/login/')
-        elif user_form.data['password'] != user_form.data['confirm_password']:
-            user_form.add_error('confirm_password', 'The passwords do not match')
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+                profile.save()
+                registered = True
         else:
-                print(user_form.errors)
-
-
+                print(user_form.errors, profile_form.errors)
     else:
             user_form = forms.UserForm()
-
-    return render(request, "kreiva/registration.html", {'registered': registered, 'user_form': user_form
-                                                            })
+            profile_form = forms.UserProfileInfoForm()
+    return render(request, "kreiva/registration.html", {'registered': registered, 'user_form': user_form,
+                                                                'profile_form': profile_form})
 
 def user_login(request):
     if request.method == 'POST':
