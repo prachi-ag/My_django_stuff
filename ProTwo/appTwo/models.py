cb511074426f66e0040d2b6767817f55c7e47462
Prachi <prachiagarwal30111998@gmail.com>
2018-09-28 15:25:12 +0530

    Removed

--- ProTwo/appTwo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    first_name = models.CharField(max_length=256)
-    last_name = models.CharField(max_length=256)
-    email = models.EmailField(max_length=256, unique=True)
--- ProTwo/appTwo/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-from appTwo import views
-
-urlpatterns = [url(r'^$', views.user, name='user ')
-               ]
