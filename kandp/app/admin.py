0cc4011f81e7217396af48faeeb8c0e7fcdda8ab
Prachi <prachiagarwal30111998@gmail.com>
2018-09-28 15:23:33 +0530

    Removed

--- kandp/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Topic, AccessRecord,Webpage
-# Register your models here.
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(AccessRecord)
--- kandp/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from app import views
-
-
-urlpatterns = [
-
-    url(r'^$', views.index1,name="index")
-]
