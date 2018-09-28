69aa6701b2c76f28169d1741b20128ea99d47bf2
Prachi <prachiagarwal30111998@gmail.com>
2018-09-28 15:25:50 +0530

    Removed

--- basicforms/basicapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasicappConfig(AppConfig):
-    name = 'basicapp'
--- basicforms/basicapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class FormName(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
